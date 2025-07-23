3a5b930b766872909969472a22301f43ec82cf45
Suraj Kumar <surajkumar06174@gmail.com>
2025-07-24 03:21:18 +0530

    feat: Add voting functionality and vote count retrieval for candidates

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -100,4 +100,69 @@ router.delete("/:candidateID", jwtAuthMiddleware, async (req, res) => {
   }
 });
 
+// let's start voting
+router.post("/vote/:candidateID", jwtAuthMiddleware, async (req, res)=>{
+  const candidateID = req.params.candidateID;
+  const userId = req.user.id;
+
+  try{
+
+    //Find the candidate with the specified candidateId
+    const candidate = await Candidate.findById(candidateID);
+    if(!candidate){
+      return res.status(404).json({message: "Candidate not found"});
+    }
+
+    // Find the user by userId
+    const user = await User.findById(userId);
+    if(!user){
+      return res.status(404).json({message: "User not found"});
+    }
+
+    if(user.isVoted){
+      res.status(400).json({message: "You have already voted"});
+    }
+
+    if(user.role == "admin"){
+      res.status(403).json({message: "Admin is not allowed"});
+    }
+
+     //Update the candidate document to record the vote
+     candidate.votes.push({userId});
+     candidate.voteCount++;
+     await candidate.save();
+
+     // Update the user document
+     user.isVoted = true;
+     await user.save();
+
+     res.status(200).json({message: "Vote recorded successfully"});
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+ // Vote Count
+ router.get('/vote/count', async (req, res)=>{
+  try{
+    //Find all candidates and sort them by voteCount in descending order
+    const candidate = await Candidate.find().sort({voteCount: 'desc'});
+
+    //Map the candidates to only return their name and voteCount
+    const voteRecord = candidate.map((data)=>{
+      return{
+        party: data.party,
+        count: data.voteCount
+      }
+    });
+    return res.status(200).json({voteRecord});
+
+  } catch(err){
+    console.log(err);
+    res.status(500).json({error: "Internal Server Error"});
+  }
+ });
+
 module.exports = router;
